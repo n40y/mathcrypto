def431be04eaa0d93be3ac25e44b99abe23714f1
N40y <131626661+n40y@users.noreply.github.com>
2026-08-01 22:50:21 +0200

    Update mathcrypto.nim

--- src/mathcrypto.nim
+++ src/mathcrypto.nim
@@ -1,9 +1,14 @@
 # src/mathcrypto.nim
 
-import ./mathcrypto/jacobi
-import ./mathcrypto/primality
-import ./mathcrypto/gf2
+import mathcrypto/jacobi
+import mathcrypto/primality
+import mathcrypto/gf2
+import mathcrypto/aes
+import mathcrypto/euclidean
+
 
 export jacobi
 export primality
 export gf2
+export aes
+export euclidean
