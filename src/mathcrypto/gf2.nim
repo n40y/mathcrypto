ba950ad3bd3a9ad8d69a6ec615c440fe549b0b56
N40y <131626661+n40y@users.noreply.github.com>
2026-08-01 23:21:50 +0200

    Update gf2.nim

--- src/mathcrypto/gf2.nim
+++ src/mathcrypto/gf2.nim
@@ -22,12 +22,13 @@ func polyMulZ2*(a, b: uint): uint {.inline.} =
     ## Polynomials are represented as integers where bit `i`
     ## corresponds to the coefficient of `X^i`.
     ## 
-    ## runnableExamples:
-    ##    #(X² + X + 1) * (X + 1) = X³ + 1
-    ##    assert polyMulZ2(7, 3) == 9
+    runnableExamples:
+        # (X² + X + 1) * (X + 1) = X³ + 1
+        assert polyMulZ2(7, 3) == 9
     ##
-    ##    #(X² + 1) * (X² + 1) = X⁴ + 1  (since 2X² = 0 mod 2)
-    ##    assert polyMulZ2(5, 5) == 17
+        # (X² + 1) * (X² + 1) = X⁴ + 1  (since 2X² = 0 mod 2)
+        assert polyMulZ2(5, 5) == 17
+    
     var a = a
     var b = b
     while b > 0:
@@ -42,10 +43,11 @@ func polyModZ2*(r: uint, m: uint = 0x11B): uint {.inline.} =
     ## Reduces polynomial `r` modulo `m` over `Z₂[X]`.
     ## Defaults to `m = 0x11B` (`X^8 + X^4 + X^3 + X + 1`, the AES irreducible polynomial).
     ##
-    ## runnableExamples:
-    ##    #(X³ + 1) mod (X³ + X + 1) => 9 mod 11 = 2 (polynomial X)
-    ##    import std/bitops
-    ##    assert polyModZ2(9, 11) == 2
+    runnableExamples:
+        # (X³ + 1) mod (X³ + X + 1) => 9 mod 11 = 2 (polynomial X)
+        import std/bitops
+        assert polyModZ2(9, 11) == 2
+    
     var r = r
     while r.bitLen >= m.bitLen:
         let k = r.bitLen - m.bitLen
@@ -55,8 +57,8 @@ func polyModZ2*(r: uint, m: uint = 0x11B): uint {.inline.} =
 func gf28Mul*(a, b: uint, m: uint = 0x11B): uint {.inline.} =
     ## Multiplies two elements `a` and `b` in the finite field `GF(2⁸)`
     ## modulo the irreducible polynomial `m` (0x11B by default for AES).
-    ##
-    ## runnableExamples:
-    ##   #Multiply 0x57 by 0x83 in GF(2⁸) with AES polynomial (expected result: 0xC1)
-    ##   assert gf28Mul(0x57, 0x83) == 0xC1
+    runnableExamples:
+        # Multiply 0x57 by 0x83 in GF(2⁸) with AES polynomial (expected result: 0xC1)
+        assert gf28Mul(0x57, 0x83) == 0xC1
+    
     return polyModZ2(polyMulZ2(a, b), m)
