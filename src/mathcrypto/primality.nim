1b5748babc26ba288731c0aab08942487dcb0899
N40y <131626661+n40y@users.noreply.github.com>
2026-08-01 23:23:29 +0200

    Update primality.nim

--- src/mathcrypto/primality.nim
+++ src/mathcrypto/primality.nim
@@ -6,9 +6,10 @@ import ./jacobi
 proc powerMod*(base, exp, m: uint64): uint64 =
   ## Computes `(base^exp) mod m` using binary exponentiation.
   ##
-  ## runnableExamples:
-  ##  assert powerMod(2'u64, 10'u64, 1000'u64) == 24'u64
-  ##  assert powerMod(5'u64, 3'u64, 13'u64) == 8'u64
+  runnableExamples:
+    assert powerMod(2'u64, 10'u64, 1000'u64) == 24'u64
+    assert powerMod(5'u64, 3'u64, 13'u64) == 8'u64
+  
   var res = 1'u64
   var b = base mod m
   var e = exp
@@ -24,10 +25,11 @@ proc isPrimeMillerRabin*(n: uint64): bool =
   ##
   ## Deterministic for all 64-bit unsigned integers (`uint64`).
   ##
-  ## runnableExamples:
-  ##  assert isPrimeMillerRabin(2'u64) == true
-  ##  assert isPrimeMillerRabin(104729'u64) == true
-  ##  assert isPrimeMillerRabin(561'u64) == false
+  runnableExamples:
+    assert isPrimeMillerRabin(2'u64) == true
+    assert isPrimeMillerRabin(104729'u64) == true
+    assert isPrimeMillerRabin(561'u64) == false
+  
   if n < 2'u64: return false
   if n == 2'u64 or n == 3'u64: return true
   if n mod 2'u64 == 0'u64: return false
@@ -61,9 +63,10 @@ proc isPrimeSolovayStrassen*(n: int, k: int = 20): bool =
   ##
   ## Probabilistic test based on Euler's criterion and the Jacobi symbol.
   ##
-  ## runnableExamples:
-  ##  assert isPrimeSolovayStrassen(104729, k = 20) == true
-  ##  assert isPrimeSolovayStrassen(561, k = 20) == false
+  runnableExamples:
+    assert isPrimeSolovayStrassen(104729, k = 20) == true
+    assert isPrimeSolovayStrassen(561, k = 20) == false
+  
   if n < 2: return false
   if n == 2: return true
   if n mod 2 == 0: return false
